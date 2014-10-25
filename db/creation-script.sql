d0e5f890379dbd43df8dc4a44bb4070f0db8f575
Les <trilinear.nz@gmail.com>
2014-10-25 23:55:11 +1200

    Updated DB scripts to reflect new ShortName column

--- db/creation-script.sql
+++ db/creation-script.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `article` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Title` varchar(255) NOT NULL,
+  `ShortName` varchar(255) NOT NULL,
   `Content` text NOT NULL,
   `Created` datetime NOT NULL,
   `Modified` datetime NOT NULL,
--- db/example-article.sql
+++ db/example-article.sql
@@ -1,10 +1,12 @@
 INSERT INTO `blog`.`article`
 (`Title`,
+`ShortName`,
 `Content`,
 `Created`,
 `Modified`)
 VALUES
 ('Welcome',
+'welcome',
 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
 '2014-10-22 22:35',
 '2014-10-22 22:35');
